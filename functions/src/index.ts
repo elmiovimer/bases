dbba3c7c7c9f9eb7484c236f66e67315f0478d19
Vimer armando Fabian <vimerarmandofabian@mac.mynetworksettings.com>
2025-03-11 14:47:08 -0400

    03112025 0214001

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import {onCall, onRequest, HttpsError} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
@@ -114,6 +114,49 @@ export const createUser = onRequest({cors: true}, async (request, response) => {
 
 
 });
+
+export const setClaim = onRequest({cors: true}, async (request, response) => {
+  console.log(' setClaim  -> ', request.body);
+  // validaciones
+  const body:  ModelsFunctions.RequestSetRol = request.body;
+  const roles = body.roles;
+  const uid = body.uid
+  const claims = {
+    roles: roles,
+  }
+  await auth.setCustomUserClaims(uid, claims);
+  console.log('set claim con éxito');
+
+  response.send({ok: true})
+})
+
+export const appCall = onCall(async (request) => {
+  console.log('user ->', request.auth);
+  let valid = false;
+  valid = await isRol(request.auth.uid, ['admin']);
+
+  if(valid){
+    console.log('hacer la funcion');
+  }
+})
+
+export const isRol = async ( uid : string, roles : string[]) =>{
+  const doc = await firestore.doc(`Users/${uid}`).get();
+  let valid = false;
+  if (doc.exists) {
+    const data : any = doc.data();
+    roles.every( rol =>{
+      if (data.roles[rol] == true) {
+        valid = true;
+        return false;
+      }
+      return true;
+    })
+
+  }
+  return valid;
+
+}
 
 // esqueleto de function
 export const functionName = onRequest(async (request, response) =>{
